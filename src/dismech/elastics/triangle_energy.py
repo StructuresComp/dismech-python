02a16e604e11f45ef364c53a70530db466933eb7
ryanchaiyakul <ryanchaiyakul@gmail.com>
2025-03-09 18:17:07 -0700

    working midedge grad_hess_energy
    The implementation is not very inefficient and very broken right now.
    But it works!

--- src/dismech/elastics/triangle_energy.py
+++ src/dismech/elastics/triangle_energy.py
@@ -59,6 +59,24 @@ class TriangleEnergy(ElasticEnergy):
         """
         factor = np.einsum('njk,njl->nlk', tau, t) / (2 * self._A)[:, None, None]
         return factor[..., None] * unit_norm[:, None, None, :]
+    
+    def _ddelfi_by_del_p_k1_p_k2(self, t, tau, unit_norm):
+        """
+        Returns a N x 3 x 3 x 3 x 3 x 3 tensor where:
+        - [N, i, j, k] corresponds to the 3x3 Hessian matrix for parameters (k1=i, k2=j)
+        and tau column k.
+        """
+        t_dot_tau = np.einsum('nij,nik->njk', t, tau)
+        scale = 1 / (4 * self._A ** 2)
+
+        # TODO: Find a more efficient way to do this :(
+        outer_1 = np.einsum('ni,nj->nij', unit_norm, t[..., 0])
+        outer_2 = np.einsum('ni,nj->nij', unit_norm, t[..., 1])
+        outer_3 = np.einsum('ni,nj->nij', unit_norm, t[..., 2])
+        outer = np.stack([outer_1, outer_2, outer_3], axis=1)
+        outer_sum = outer + outer.transpose(0, 1, 3, 2)
+
+        return np.einsum('nij,nklm,n->nikjlm', t_dot_tau, outer_sum, scale)
 
     # FIXME: Override main function as strain is difficult to isolate right now
 
@@ -102,6 +120,7 @@ class TriangleEnergy(ElasticEnergy):
         delfi = self._delfi_by_delpk(t, tau, unit_norm)
 
         # useful broadcasts
+        # FIXME: SUPER WRONG!!! I AM STUPID
         s_xis_i = s_xis[:, None, :]
         s_xis_j = s_xis[:, :, None]
         s_initxis_j = s_initxis[:, :, None]
@@ -132,9 +151,9 @@ class TriangleEnergy(ElasticEnergy):
 
             return M11 + M12 + M13, M21 + M22 + M23
 
-        G11, G21 = compute_G_block(delfi[:,0])
-        G12, G22 = compute_G_block(delfi[:,1])
-        G13, G23 = compute_G_block(delfi[:,2])
+        G11, G21 = compute_G_block(delfi[:, 0])
+        G12, G22 = compute_G_block(delfi[:, 1])
+        G13, G23 = compute_G_block(delfi[:, 2])
 
         ci_sj = 2 * np.einsum('ni,ni->ni', c, self._s_s)[:, None, :]
 
@@ -158,12 +177,92 @@ class TriangleEnergy(ElasticEnergy):
             ((1-self._nu)[..., None] * grad_e1 +
              self._nu[..., None]*grad_e2) * self._A[..., None]
 
+        # Hessian - nightmare :(
+
+        
+        # print(ddelfi[0, 0, 0, 0])
+
+        # 1: d/dx^2 super easy (N x 3 x 3)
+        si_sj = np.einsum('ni, nj->nij', self._s_s, self._s_s)
+
+        tri_inds = np.triu_indices(3)
+        common = 2 * ci_cj[:, tri_inds] * si_sj[:, tri_inds]
+
+        def compute_dxdx(factor):
+            """ Compute upper triangle (66% computation) """
+            dxdx_tri = common * factor[:, tri_inds]
+
+            dxdx = np.zeros((N, 3, 3))
+            dxdx[:, tri_inds] = dxdx_tri
+            dxdx[:, tri_inds] = dxdx_tri
+            return dxdx
+
+        dxdx_1 = compute_dxdx(t_dot_t_sq)
+        dxdx_2 = compute_dxdx(t_norms_sq_sq)
+
+        # 2: d/dp^2 HELLL (N x 9 x 9)
+
+        # [N, i, j, k, a, b] corresponds to the a x b Hessian matrix for parameters (k1=i, k2=j) and tau column k.
+        ddelfi = self._ddelfi_by_del_p_k1_p_k2(t, tau, unit_norm)
+
+        # delfi_sq is a N x 3 x 3 x 3 x 3 x 3 x 3 where t_i, tau_j, t_k, tau_l returns delfi(t_i, tau_j).T * delfi(t_k, tau_l)
+        delfi_sq = np.einsum('nabc,ndef->nabdecf', delfi, delfi)
+
+        # Not using above because the broadcast was wrong
+
+        common_1 = -ci_cj * s_xis[:, :, None]
+        common_3 = -ci_cj * s_xis[:, None, :]
+        common_5 = 2 * ci_init_cj * s_initxis[:, None, :]
+
+        def compute_dpdp(factor1, factor2):
+            # -ci_cj * s_xis_j * factor1 * ddelfi[k2, k1] summed over i,j
+            M1 = np.einsum('nij,nij,ntkjab->ntakb', common_1,factor1, ddelfi)
+            # -ci_cj * factor1 * (delfi_j_k1.T @ delfi_i_k2)
+            M2 = np.einsum('nij,nij,nkjtiab->ntakb', ci_cj, factor1, delfi_sq)
+            # -ci_cj * s_xis_i * factor1 * ddelfi[k2, k1] summed over i,j
+            M3 = np.einsum('nij,nij,ntkiab->ntakb', common_3, factor1, ddelfi)
+            # -ci_cj * factor1 * (delfi_i_k1.T @ delfi_j_k2) summed over i,j
+            M4 = np.einsum('nij,nij,nkitjab->ntakb', ci_cj, factor1, delfi_sq)
+            # Term M5: 2 * ci_init_cj * factor2 * s_initxis_j * ddelfi[k2, k1] summed over i,j
+            M5 = np.einsum('nij,nij,ntkiab->ntakb', common_5, factor2, ddelfi)
+
+            return (M1 + M2 + M3 + M4 + M5).reshape(N, 9, 9)
+        
+        dpdp_1 = compute_dpdp(t_dot_t_sq, t_dot_init_t_sq)
+        dpdp_2 = compute_dpdp(t_norms_sq_sq, t_norms_sq_ls_sq)
+        
+        
+
+        # 3: d/dxdp meh (N x 9 x 3)
+
+        common = -2 * c[:, :, None] * self._s_s[:, :, None] * c[:, None, :]
+
+        def compute_dxdp(factor):
+            """Compute a N x 3 x 9 matrix (for upper triangle)"""
+            return np.einsum('nij,nljc->nlci', common * factor, delfi).reshape(-1,9,3)
+
+        dxdp_1 = compute_dxdp(t_dot_t_sq)
+        dxdp_2 = compute_dxdp(t_norms_sq_sq)
+
+        # 4: put together the hessian
+
+        def get_hess_blocks(TL, TR, BR):
+            return np.concatenate([np.concatenate([TL, TR], axis=2), np.concatenate([np.transpose(TR, (0, 2, 1)), BR], axis=2)], axis=1)
+
+        hess_e1 = get_hess_blocks(dpdp_1, dxdp_1, dxdx_1)
+        hess_e2 = get_hess_blocks(dpdp_2, dxdp_2, dxdx_2)
+
+        hessE_with_stiff = self._kb[..., None, None] * \
+            ((1-self._nu)[..., None, None] * hess_e1 +
+             self._nu[..., None, None]*hess_e2) * self._A[..., None, None]
+
         # Accumulate into global array
         n_dof = state.q.shape[0]
         Fs = np.zeros(n_dof)
         Js = np.zeros((n_dof, n_dof))
+
         np.add.at(Fs, self._ind, -gradE_with_stiff)
-        # Hessian accumulation would follow similar logic
-        Js = np.zeros((n_dof, n_dof))
+        np.add.at(Js, (self._ind[:, :, None],
+                  self._ind[:, None, :]), -hessE_with_stiff)
 
         return Fs, Js

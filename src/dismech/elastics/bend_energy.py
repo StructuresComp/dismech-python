3c2d4d07dd5995bc34f9aa6b52e66b734b0d9237
ryanchaiyakul <ryanchaiyakul@gmail.com>
2025-04-08 14:03:15 -0700

    switched to local vars
    Switched from in-place operations to local vars for future JAX testing.

--- src/dismech/elastics/bend_energy.py
+++ src/dismech/elastics/bend_energy.py
@@ -26,53 +26,39 @@ class BendEnergy(ElasticEnergy):
         self._sign_hess = self._sign_grad[:, :, None] * \
             self._sign_grad[:, None, :]
 
-        self.m1e = np.empty((N, 3))
-        self.m2e = np.empty((N, 3))
-        self.m1f = np.empty((N, 3))
-        self.m2f = np.empty((N, 3))
-
-        self.ee = np.empty((N, 3))
-        self.ef = np.empty((N, 3))
-        self.norm_e = np.empty((N, 1))
-        self.norm_f = np.empty((N, 1))
-
-        self.tf = np.empty((N, 3))
-        self.te = np.empty((N, 3))
-
-        self.kappa = np.empty((N, 2))
-
-    def _set_adjusted_material_directors(self, m1: np.ndarray, m2: np.ndarray):
-        self.m1e[:] = m1[self._edges_ind[:, 0]]
-        self.m2e[:] = m2[self._edges_ind[:, 0]] * self._sgn[:, 0, None]
-        self.m1f[:] = m1[self._edges_ind[:, 1]]
-        self.m2f[:] = m2[self._edges_ind[:, 1]] * self._sgn[:, 1, None]
+    def _get_adjusted_material_directors(self, m1: np.ndarray, m2: np.ndarray):
+        m1e = m1[self._edges_ind[:, 0]]
+        m2e = m2[self._edges_ind[:, 0]] * self._sgn[:, 0, None]
+        m1f = m1[self._edges_ind[:, 1]]
+        m2f = m2[self._edges_ind[:, 1]] * self._sgn[:, 1, None]
+        return m1e, m2e, m1f, m2f
 
     def get_strain(self, state: RobotState) -> np.ndarray:
         n0p, n1p, n2p = self._get_node_pos(state.q)
-        self._set_adjusted_material_directors(state.m1, state.m2)
+        m1e, m2e, m1f, m2f = self._get_adjusted_material_directors(state.m1, state.m2)
 
         # Precompute common terms
-        np.subtract(n1p, n0p, out=self.ee)
-        np.subtract(n2p, n1p, out=self.ef)
-        self.norm_e[:] = np.linalg.norm(self.ee, axis=1, keepdims=True)
-        self.norm_f[:] = np.linalg.norm(self.ef, axis=1, keepdims=True)
-        np.divide(self.ee, self.norm_e, self.te)
-        np.divide(self.ef, self.norm_f, self.tf)
+        ee = n1p - n0p
+        ef = n2p - n1p
+        norm_e = np.linalg.norm(ee, axis=1)
+        norm_f = np.linalg.norm(ef, axis=1)
+        te = ee / norm_e[:, None]
+        tf = ef / norm_f[:, None]
 
-        chi = 1.0 + np.sum(self.te * self.tf, axis=1)
+        chi = 1.0 + np.sum(te * tf, axis=1)
         chi_inv = 1.0 / chi
-        kb = 2.0 * np.cross(self.te, self.tf) * chi_inv[:, None]
+        kb = 2.0 * np.cross(te, tf) * chi_inv[:, None]
 
-        # Curvatures
-        self.kappa[:, 0] = 0.5 * np.sum(kb * (self.m2e + self.m2f), axis=1)
-        self.kappa[:, 1] = -0.5 * np.sum(kb * (self.m1e + self.m1f), axis=1)
+        kappa1 = 0.5 * np.sum(kb * (m2e + m2f), axis=1)
+        kappa2 = -0.5 * np.sum(kb * (m1e + m1f), axis=1)
 
-        return self.kappa
+        return np.stack([kappa1, kappa2], axis=-1)
 
     def grad_hess_strain(self, state: RobotState) -> typing.Tuple[np.ndarray, np.ndarray]:
         n0p, n1p, n2p = self._get_node_pos(state.q)
-        self._set_adjusted_material_directors(state.m1, state.m2)
+        m1e, m2e, m1f, m2f = self._get_adjusted_material_directors(state.m1, state.m2)
         n_springs = n0p.shape[0]
+        
         Id3 = np.eye(3)[None, :, :]  # For broadcasting
 
         # Precompute common terms
@@ -88,12 +74,12 @@ class BendEnergy(ElasticEnergy):
         kb = 2.0 * np.cross(te, tf) * chi_inv[:, None]
 
         tilde_t = (te + tf) * chi_inv[:, None]
-        tilde_d1 = (self.m1e + self.m1f) * chi_inv[:, None]
-        tilde_d2 = (self.m2e + self.m2f) * chi_inv[:, None]
+        tilde_d1 = (m1e + m1f) * chi_inv[:, None]
+        tilde_d2 = (m2e + m2f) * chi_inv[:, None]
 
         # Curvatures
-        kappa1 = 0.5 * np.sum(kb * (self.m2e + self.m2f), axis=1)
-        kappa2 = -0.5 * np.sum(kb * (self.m1e + self.m1f), axis=1)
+        kappa1 = 0.5 * np.sum(kb * (m2e + m2f), axis=1)
+        kappa2 = -0.5 * np.sum(kb * (m1e + m1f), axis=1)
 
         # First derivatives
         Dkappa1De = (1.0 / norm_e[:, None]) * \
@@ -115,10 +101,10 @@ class BendEnergy(ElasticEnergy):
         gradKappa[:, 6:9, 1] = Dkappa2Df
 
         # Twist terms
-        gradKappa[:, 9, 0] = -0.5 * np.sum(kb * self.m1e, axis=1)
-        gradKappa[:, 10, 0] = -0.5 * np.sum(kb * self.m1f, axis=1)
-        gradKappa[:, 9, 1] = -0.5 * np.sum(kb * self.m2e, axis=1)
-        gradKappa[:, 10, 1] = -0.5 * np.sum(kb * self.m2f, axis=1)
+        gradKappa[:, 9, 0] = -0.5 * np.sum(kb * m1e, axis=1)
+        gradKappa[:, 10, 0] = -0.5 * np.sum(kb * m1f, axis=1)
+        gradKappa[:, 9, 1] = -0.5 * np.sum(kb * m2e, axis=1)
+        gradKappa[:, 10, 1] = -0.5 * np.sum(kb * m2f, axis=1)
 
         # Second derivatives
         norm2_e = norm_e**2
@@ -142,7 +128,7 @@ class BendEnergy(ElasticEnergy):
         tf_c_d2t = np.cross(tf, tilde_d2)
         tf_c_d2t_o_tt = batch_outer(tf_c_d2t, tilde_t)
         tt_o_tf_c_d2t = batch_outer(tilde_t, tf_c_d2t)
-        kb_o_d2e = batch_outer(kb, self.m2e)
+        kb_o_d2e = batch_outer(kb, m2e)
 
         D2kappa1De2 = (1/norm2_e[:, None, None])*(2*kappa1[:, None, None]*tt_o_tt - tf_c_d2t_o_tt - tt_o_tf_c_d2t) \
             - (kappa1[:, None, None]/(chi[:, None, None]*norm2_e[:, None, None]))*(Id3 - batch_outer(te, te)) \
@@ -151,7 +137,7 @@ class BendEnergy(ElasticEnergy):
         te_c_d2t = np.cross(te, tilde_d2)
         te_c_d2t_o_tt = batch_outer(te_c_d2t, tilde_t)
         tt_o_te_c_d2t = batch_outer(tilde_t, te_c_d2t)
-        kb_o_d2f = batch_outer(kb, self.m2f)
+        kb_o_d2f = batch_outer(kb, m2f)
 
         D2kappa1Df2 = (1/norm2_f[:, None, None])*(2*kappa1[:, None, None]*tt_o_tt + te_c_d2t_o_tt + tt_o_te_c_d2t) \
             - (kappa1[:, None, None]/(chi[:, None, None]*norm2_f[:, None, None]))*(Id3 - batch_outer(tf, tf)) \
@@ -166,7 +152,7 @@ class BendEnergy(ElasticEnergy):
         tf_c_d1t = np.cross(tf, tilde_d1)
         tf_c_d1t_o_tt = batch_outer(tf_c_d1t, tilde_t)
         tt_o_tf_c_d1t = batch_outer(tilde_t, tf_c_d1t)
-        kb_o_d1e = batch_outer(kb, self.m1e)
+        kb_o_d1e = batch_outer(kb, m1e)
 
         D2kappa2De2 = (1/norm2_e[:, None, None])*(2*kappa2[:, None, None]*tt_o_tt + tf_c_d1t_o_tt + tt_o_tf_c_d1t) \
             - (kappa2[:, None, None]/(chi[:, None, None]*norm2_e[:, None, None]))*(Id3 - batch_outer(te, te)) \
@@ -175,7 +161,7 @@ class BendEnergy(ElasticEnergy):
         te_c_d1t = np.cross(te, tilde_d1)
         te_c_d1t_o_tt = batch_outer(te_c_d1t, tilde_t)
         tt_o_te_c_d1t = batch_outer(tilde_t, te_c_d1t)
-        kb_o_d1f = batch_outer(kb, self.m1f)
+        kb_o_d1f = batch_outer(kb, m1f)
 
         D2kappa2Df2 = (1/norm2_f[:, None, None])*(2*kappa2[:, None, None]*tt_o_tt - te_c_d1t_o_tt - tt_o_te_c_d1t) \
             - (kappa2[:, None, None]/(chi[:, None, None]*norm2_f[:, None, None]))*(Id3 - batch_outer(tf, tf)) \
@@ -186,58 +172,58 @@ class BendEnergy(ElasticEnergy):
                                                                  + tf_c_d1t_o_tt - tt_o_te_c_d1t + cross_mat_batch(tilde_d1))
 
         # Twist terms
-        D2kappa1Dthetae2 = -0.5 * np.sum(kb * self.m2e, axis=1)
-        D2kappa1Dthetaf2 = -0.5 * np.sum(kb * self.m2f, axis=1)
-        D2kappa2Dthetae2 = 0.5 * np.sum(kb * self.m1e, axis=1)
-        D2kappa2Dthetaf2 = 0.5 * np.sum(kb * self.m1f, axis=1)
+        D2kappa1Dthetae2 = -0.5 * np.sum(kb * m2e, axis=1)
+        D2kappa1Dthetaf2 = -0.5 * np.sum(kb * m2f, axis=1)
+        D2kappa2Dthetae2 = 0.5 * np.sum(kb * m1e, axis=1)
+        D2kappa2Dthetaf2 = 0.5 * np.sum(kb * m1f, axis=1)
 
         # Coupled terms
         D2kappa1DeDthetae = (1/norm_e[:, None, None]) * (
-            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, self.m1e),
+            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, m1e),
                             tilde_t[:, :, None])
-            - (1/chi[:, None, None]) * np.cross(tf, self.m1e)[:, :, None]
+            - (1/chi[:, None, None]) * np.cross(tf, m1e)[:, :, None]
         )
 
         D2kappa1DeDthetaf = (1/norm_e[:, None, None]) * (
-            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, self.m1f),
+            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, m1f),
                             tilde_t[:, :, None])
-            - (1/chi[:, None, None]) * np.cross(tf, self.m1f)[:, :, None]
+            - (1/chi[:, None, None]) * np.cross(tf, m1f)[:, :, None]
         )
 
         D2kappa1DfDthetae = (1/norm_f[:, None, None]) * (
-            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, self.m1e),
+            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, m1e),
                             tilde_t[:, :, None])
-            + (1/chi[:, None, None]) * np.cross(te, self.m1e)[:, :, None]
+            + (1/chi[:, None, None]) * np.cross(te, m1e)[:, :, None]
         )
 
         D2kappa1DfDthetaf = (1/norm_f[:, None, None]) * (
-            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, self.m1f),
+            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, m1f),
                             tilde_t[:, :, None])
-            + (1/chi[:, None, None]) * np.cross(te, self.m1f)[:, :, None]
+            + (1/chi[:, None, None]) * np.cross(te, m1f)[:, :, None]
         )
 
         D2kappa2DeDthetae = (1/norm_e[:, None, None]) * (
-            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, self.m2e),
+            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, m2e),
                             tilde_t[:, :, None])
-            - (1/chi[:, None, None]) * np.cross(tf, self.m2e)[:, :, None]
+            - (1/chi[:, None, None]) * np.cross(tf, m2e)[:, :, None]
         )
 
         D2kappa2DeDthetaf = (1/norm_e[:, None, None]) * (
-            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, self.m2f),
+            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, m2f),
                             tilde_t[:, :, None])
-            - (1/chi[:, None, None]) * np.cross(tf, self.m2f)[:, :, None]
+            - (1/chi[:, None, None]) * np.cross(tf, m2f)[:, :, None]
         )
 
         D2kappa2DfDthetae = (1/norm_f[:, None, None]) * (
-            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, self.m2e),
+            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, m2e),
                             tilde_t[:, :, None])
-            + (1/chi[:, None, None]) * np.cross(te, self.m2e)[:, :, None]
+            + (1/chi[:, None, None]) * np.cross(te, m2e)[:, :, None]
         )
 
         D2kappa2DfDthetaf = (1/norm_f[:, None, None]) * (
-            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, self.m2f),
+            0.5 * np.einsum('sij,sjk->sik', batch_outer(kb, m2f),
                             tilde_t[:, :, None])
-            + (1/chi[:, None, None]) * np.cross(te, self.m2f)[:, :, None]
+            + (1/chi[:, None, None]) * np.cross(te, m2f)[:, :, None]
         )
 
         def batch_assign_blocks(DDkappa, D2De2, D2DeDf, D2DfDe, D2Df2, D2t1, D2t2, D2ct):
